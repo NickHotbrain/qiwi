342f7957499ec7acac09587d29015e84224ad20b
Nikita <nikita@hotbrains.ru>
2018-08-01 11:43:10 +0700

    + created base classes and composer.json

---
+++ Qiwi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NickHotbrain\WalletApi;
+
+
+class Qiwi
+{
+    public function __construct()
+    {
+
+    }
+}
---
+++ ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace NickHotbrain\WalletApi;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        include_once('helpers.php');
+
+//        $this->loadViewsFrom(__DIR__.'/../views', 'wallet-api');
+
+//        $this->publishes([
+//            __DIR__.'/../views' => base_path('resources/views/vendor/wallet-api'),
+//        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
